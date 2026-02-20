a33ea24d260229c080b9140b9156b91b2387bd8c
trxeblxe <sahil.srivastava_cs.aiml24@gla.ac.in>
2026-02-20 09:22:57 +0000

    implement ultrasonic distance measurement and servo control

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,43 @@
  * Accepts angle input via Serial Monitor and validates range (0–180).
  * */
 #include <Arduino.h>
+
+#include <Servo.h>
+
+Servo MYSERVO;
+
+int trigPin = 9;
+int echoPin = 10;
+
 void setup() {
-// write your initialization code here
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    MYSERVO.attach(5);
+    Serial.begin(9600);
 }
 
-
 void loop() {
-// write your code here
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(trigPin, LOW);
+
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = (duration * 0.0343) / 2;
+
+    if (distance <= 5) {
+        MYSERVO.write(180);   // rotate servo
+    } else {
+        MYSERVO.write(0);     // reset servo
+    }
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    delay(500);
 }
